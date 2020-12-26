114f6d8400bc35b49a55c6ff837e1b1a7076a561
Javier Toja <javier.toja.alamancos@gmail.com>
2020-12-26 12:18:20 +0100

    Baisc bot example

---
+++ nodejs/TestingBot/index.js
@@ -0,0 +1,8 @@
+require('dotenv').config();
+
+const Telegraf = require('telegraf') // import telegram lib
+
+const bot = new Telegraf(process.env.BOT_TOKEN) // get the token from envirenment variable
+bot.start((ctx) => ctx.reply('Welcome')) // display Welcome text when we start bot
+bot.hears('hi', (ctx) => ctx.reply('Hey there you')) // listen and handle when user type hi text
+bot.launch() // start
