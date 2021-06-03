40bb8c0db90802f252bf75e901746300585589a6
Javier Toja Alamancos <javier.toja.alamancos@gmail.com>
2021-06-03 18:59:06 +0200

    Delete old things

--- quarkus/meteoexplorer/src/test/java/gal/jtoja/NativeHelloResourceIT.java
+++
@@ -1,9 +0,0 @@
-package gal.jtoja;
-
-import io.quarkus.test.junit.NativeImageTest;
-
-@NativeImageTest
-public class NativeHelloResourceIT extends HelloResourceTest {
-
-    // Execute the same tests but in native mode.
-}
