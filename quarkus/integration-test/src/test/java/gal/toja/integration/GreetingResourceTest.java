6102353d22c84167704300e0fad2ecb99344c7bc
Javier Toja Alamancos <javier.toja.alamancos@gmail.com>
2021-06-09 19:32:53 +0200

    Try verify with no unit tests

--- quarkus/integration-test/src/test/java/gal/toja/integration/GreetingResourceTest.java
+++ quarkus/integration-test/src/test/java/gal/toja/integration/GreetingResourceTest.java
@@ -1,12 +1,15 @@
 package gal.toja.integration;
 
 import io.quarkus.test.junit.QuarkusTest;
+
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
 
 @QuarkusTest
+@Disabled
 public class GreetingResourceTest {
 
     @Test
