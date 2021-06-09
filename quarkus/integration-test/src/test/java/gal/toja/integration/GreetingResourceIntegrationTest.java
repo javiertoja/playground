ae514ffa03506255867198949e33960bfb410e40
Javier Toja Alamancos <javier.toja.alamancos@gmail.com>
2021-06-09 19:19:53 +0200

    Initial test for integration testing with quarkus

---
+++ quarkus/integration-test/src/test/java/gal/toja/integration/GreetingResourceIntegrationTest.java
@@ -0,0 +1,21 @@
+package gal.toja.integration;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.is;
+
+@QuarkusIntegrationTest
+public class GreetingResourceIntegrationTest {
+
+    @Test
+    public void testHelloEndpoint() {
+        given()
+          .when().get("/hello-resteasy")
+          .then()
+             .statusCode(200)
+             .body(is("Hello RESTEasy"));
+    }
+
+}
