385fa58865de24f083d5c696c749017875baf046
Javier Toja <javier.toja.alamancos@gmail.com>
2021-06-10 08:47:22 +0200

    Update GreetingResourceIT.java

--- quarkus/integration-test/src/test/java/gal/toja/integration/GreetingResourceIT.java
+++ quarkus/integration-test/src/test/java/gal/toja/integration/GreetingResourceIT.java
@@ -7,7 +7,7 @@ import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
 
 @QuarkusIntegrationTest
-public class GreetingResourceIntegrationTest {
+public class GreetingResourceIT {
 
     @Test
     public void testHelloEndpoint() {
